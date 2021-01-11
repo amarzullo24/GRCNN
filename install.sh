924a129e2cbf30be295ce8838810c7acf8e4eaca
AldoMarzullo <marzullo.aldo24@gmail.com>
2021-01-11 16:56:07 +0100

    minor

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-!git clone https://www.github.com/nvidia/apex && cd apex && rm -rf build && python setup.py install --cuda_ext --cpp_ext
+git clone https://www.github.com/nvidia/apex && cd apex && rm -rf build && python setup.py install --cuda_ext --cpp_ext
